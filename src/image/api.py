f89f8abf6a4a6f3253f1d5509d60c85cccbf659e
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-08-18 22:35:43 +0100

    [UPD] Implement image filter api
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

--- src/image/api.py
+++ src/image/api.py
@@ -1,18 +1,11 @@
-from urllib import request
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, send_file
 
 from flasgger import swag_from
 
-
-from src.constants.http_status_codes import (
-    HTTP_200_OK,
-    HTTP_500_INTERNAL_SERVER_ERROR,
-)
+from src.constants.http_status_codes import HTTP_500_INTERNAL_SERVER_ERROR
 from src.util.util import process_image_url
 
-image_filter_bp = Blueprint(
-    "image_filter", __name__, url_prefix="/api/v1/image-filter"
-)
+image_filter_bp = Blueprint("image_filter", __name__, url_prefix="/api/v1/image-filter")
 
 
 @image_filter_bp.get("/")
@@ -22,10 +15,11 @@ def filter_image() -> str:
     image_url = request.args.get("image_url")
 
     try:
-        filtered_image = process_image_url(image_url=image_url)
+        image = process_image_url(image_url=image_url)
+
     except:
         return jsonify(
-            {"data": filtered_image}, HTTP_500_INTERNAL_SERVER_ERROR
+            {"data": "internal server error"}, HTTP_500_INTERNAL_SERVER_ERROR
         )
 
-    return jsonify({"data": filtered_image}, HTTP_200_OK)
+    return send_file(image, mimetype="image/gif")
